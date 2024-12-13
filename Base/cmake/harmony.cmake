dda8a7225e8f63729116ea37d769cd7028384439
Reg Marr <reginald.marr@reactiondynamics.space>
2024-12-13 15:15:21 -0500

    Making the properlinkage

--- Base/cmake/harmony.cmake
+++ Base/cmake/harmony.cmake
@@ -3,20 +3,20 @@ verify_path(HARMONY_DIR "Harmony generated files")
 
 set(HARMONY_SOURCES
     # Core files
-    # ${HARMONY_DIR}/initialization.c
+    ${HARMONY_DIR}/initialization.c
     ${HARMONY_DIR}/interrupts.c
     ${HARMONY_DIR}/exceptions.c
     ${HARMONY_DIR}/libc_syscalls.c
-    # ${HARMONY_DIR}/freertos_hooks.c
+    ${HARMONY_DIR}/freertos_hooks.c
 
     # OSAL
-    # ${HARMONY_DIR}/osal/osal_freertos.c
+    ${HARMONY_DIR}/osal/osal_freertos.c
 
     # Peripheral implementations
-    # ${HARMONY_DIR}/peripheral/clk/plib_clk.c
-    # ${HARMONY_DIR}/peripheral/nvic/plib_nvic.c
-    # ${HARMONY_DIR}/peripheral/pio/plib_pio.c
-    # ${HARMONY_DIR}/peripheral/efc/plib_efc.c
+    ${HARMONY_DIR}/peripheral/clk/plib_clk.c
+    ${HARMONY_DIR}/peripheral/nvic/plib_nvic.c
+    ${HARMONY_DIR}/peripheral/pio/plib_pio.c
+    ${HARMONY_DIR}/peripheral/efc/plib_efc.c
 )
 
 # Add include directories
