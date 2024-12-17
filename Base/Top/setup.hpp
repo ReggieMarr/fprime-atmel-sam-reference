ce57b3190f2f8b3b28bd3bcc80fe85c976886c79
Reg Marr <reginald.marr@reactiondynamics.space>
2024-12-17 16:18:30 -0500

    Add fprime Cmake, port Top and LedBlinker from baremetal-reference

---
+++ Base/Top/setup.hpp
@@ -0,0 +1,12 @@
+#ifndef __SETUP_H__
+#define __SETUP_H__
+
+#include <atmelBaseTopologyAc.hpp>
+#include <atmelBaseTopology.hpp>
+
+// Used for logging
+#include <Arduino/Os/Console.hpp>
+
+bool setup();
+
+#endif // __SETUP_H__
